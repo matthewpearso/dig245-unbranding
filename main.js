f201c2a704fe291ffc5593695c304917bfe23784
Matthew Pearson <matthewpearson803@gmail.com>
2025-09-20 12:48:40 -0400

    Added a log about why I made this project

--- main.js
+++ main.js
@@ -15,3 +15,5 @@ var rightButton = document.querySelector('.right-button');
 rightButton.addEventListener("click", function() {
     alert("You've successfully contacted our greedy salesmen! Prepare for an onslaught of pushy calls, confusing pricing models, and relentless upselling that will leave your head spinning and your wallet empty. Welcome to the world of AWS!");
 });
+
+console.log("Amazon Web Services is one of the largest cloud providers in the world, with a market share of over 30%. However, their pricing can be complex and difficult to understand, and many users find themselves overpaying for services they don't need or use. Their services are incredibly expensive and have a tendency to cause small tech startup to go out of business. Additionally, AWS has been criticized for its environmental impact, as their data centers consume large amounts of energy.");
